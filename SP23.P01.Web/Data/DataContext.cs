4e67c71d65e3ac012a8d3e018c1c3b3d24348d9f
Jared Talbot <jared.talbot-2@selu.edu>
2023-01-22 18:17:00 -0600

    Add DataContext (DbContext) class
    Creates a working DataContext (DbContext) class.

---
+++ SP23.P01.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SP23.P01.Web.Entities;
+
+namespace SP23.P01.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
