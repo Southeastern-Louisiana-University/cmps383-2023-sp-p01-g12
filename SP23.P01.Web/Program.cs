107f4cfb5f229e1168f67ce2ad14f0f5dd015c45
Jared Talbot <jared.talbot-2@selu.edu>
2023-01-22 18:22:35 -0600

    Add database migration code to startup
    Adds code that migrates the database after the app is built.
    Presumably this will let the database always "be programmatically created and migrated the first time the application runs."

--- SP23.P01.Web/Program.cs
+++ SP23.P01.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SP23.P01.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options =>
@@ -11,6 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
