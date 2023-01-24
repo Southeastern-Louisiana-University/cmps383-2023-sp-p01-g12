f9e651714ce4625c277b9cb0bd68d3a30c06f50a
Zoe Klein <zoe.klein@selu.edu>
2023-01-23 20:54:24 -0600

    added in dtos for trainstation

--- SP23.P01.Web/Entities/TrainStation.cs
+++ SP23.P01.Web/Entities/TrainStation.cs
@@ -6,4 +6,32 @@
         public string? Name { get; set; }
         public string? Address { get; set; }
     }
+
+    public class TrainStationGetDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+
+    public class TrainStationCreateDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+
+    public class TrainStationUpdateDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+
+    public class TrainStationDeleteDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
 }
