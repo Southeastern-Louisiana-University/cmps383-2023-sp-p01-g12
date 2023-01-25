3d5b83584e1b38988b45ed9a2a8922e44f554ac1
Jared Talbot <jared.talbot-2@selu.edu>
2023-01-25 17:53:45 -0600

    Add TrainStation configuration

--- SP23.P01.Web/Entities/TrainStation.cs
+++ SP23.P01.Web/Entities/TrainStation.cs
@@ -1,10 +1,28 @@
-﻿namespace SP23.P01.Web.Entities
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SP23.P01.Web.Entities
 {
     public class TrainStation
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class TrainStationConfiguration : IEntityTypeConfiguration<TrainStation>
+    {
+        public void Configure(EntityTypeBuilder<TrainStation> builder)
+        {
+            builder
+                .Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(120);
+
+            builder
+                .Property(t => t.Address)
+                .IsRequired();
+        }
     }
 
     public class TrainStationGetDto
