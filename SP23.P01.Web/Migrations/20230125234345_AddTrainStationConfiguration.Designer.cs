638f307f945cd955c4825a04ae44a96aa7e2beb7
Jared Talbot <jared.talbot-2@selu.edu>
2023-01-25 17:54:20 -0600

    Add migrations (AddTrainStationConfiguration)

--- SP23.P01.Web/Migrations/20230125234345_AddTrainStationConfiguration.Designer.cs
+++ SP23.P01.Web/Migrations/20230125234345_AddTrainStationConfiguration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SP23.P01.Web.Data;
 
@@ -10,9 +11,11 @@ using SP23.P01.Web.Data;
 namespace SP23.P01.Web.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230125234345_AddTrainStationConfiguration")]
+    partial class AddTrainStationConfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
