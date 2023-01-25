3cb365605c33af10ea044c1ff084cf0570b14436
Zoe Klein <zoe.klein@selu.edu>
2023-01-25 12:33:10 -0600

    wip; finish response & working on controller

--- SP23.P01.Web/Common/Response.cs
+++ SP23.P01.Web/Common/Response.cs
@@ -1,6 +1,16 @@
-﻿namespace SP23.P01.Web.Common
+﻿using SP23.P01.Web.Entities;
+
+namespace SP23.P01.Web.Common
 {
     public class Response
     {
+        public List<TrainStationGetDto>? Data { get; set; }
+        public List<Error> Errors { get; set; } = new List<Error>();
+        public bool HasErrors => Errors.Count > 0;
+
+        public void AddError(string property, string message)
+        {
+            Errors.Add(new Error(property, message));
+        }
     }
 }
--- SP23.P01.Web/Controllers/TrainStationsController.cs
+++ SP23.P01.Web/Controllers/TrainStationsController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SP23.P01.Web.Common;
 using SP23.P01.Web.Data;
+using SP23.P01.Web.Entities;
 
 namespace SP23.P01.Web.Controllers
 {
     [ApiController]
-    [Route("/api/trainstations")]
+    [Route("/api/trainStations")]
     public class TrainStationsController : Controller
     {
         private readonly DataContext _dataContext;
@@ -13,6 +14,55 @@ namespace SP23.P01.Web.Controllers
         public TrainStationsController(DataContext dataContext)
         {
             _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult GetTrainStations()
+        {
+            var response = new Response();
+
+            var trainStationToReturn = _dataContext
+                .TrainStations
+                .Select(x => new TrainStationGetDto {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Address = x.Address,
+            })
+            .ToList();
+
+            response.Data = trainStationToReturn;
+
+            return Ok(response);
+        }
+
+        [HttpGet("{trainStationsId}")]
+        public ActionResult GetById([FromRoute] int trainStationId)
+        {
+            var response = new Response();
+
+            var trainStationFromDatabase = _dataContext.TrainStations.FirstOrDefault(x => x.Id == trainStationId);
+
+            if (trainStationFromDatabase == null)
+            {
+                response.AddError("trainStationId", "No trainStation found with that Id");
+            }
+
+            if (response.HasErrors)
+            {
+                return BadRequest(response);
+            }
+
+            var trainStationToReturn = new TrainStationGetDto
+            {
+                Id = trainStationFromDatabase.Id,
+                Name = trainStationFromDatabase.Name,
+                Address = trainStationFromDatabase.Address
+            };
+
+            //throws error?
+            //response.Data = trainStationToReturn;
+
+            return Ok(response);
         }
     }
 }
