8f61f013627ef1c33375146ba1be00f7bb51bf5b
Jared Talbot <jared.talbot-2@selu.edu>
2023-01-25 18:52:31 -0600

    Update DTOs

--- SP23.P01.Web/Entities/TrainStation.cs
+++ SP23.P01.Web/Entities/TrainStation.cs
@@ -6,8 +6,8 @@ namespace SP23.P01.Web.Entities
     public class TrainStation
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
     }
 
     public class TrainStationConfiguration : IEntityTypeConfiguration<TrainStation>
@@ -28,28 +28,19 @@ namespace SP23.P01.Web.Entities
     public class TrainStationGetDto
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
     }
 
     public class TrainStationCreateDto
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
     }
 
     public class TrainStationUpdateDto
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-    }
-
-    public class TrainStationDeleteDto
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
     }
 }
