0b37630b4c1dcfb4c798d0692a54e29bdd8590d3
Zoe Klein <zoe.klein@selu.edu>
2023-01-25 18:27:46 -0600

    wip; cleanup

--- SP23.P01.Web/Controllers/TrainStationsController.cs
+++ SP23.P01.Web/Controllers/TrainStationsController.cs
@@ -19,7 +19,6 @@ namespace SP23.P01.Web.Controllers
         [HttpGet]
         public ActionResult GetTrainStations()
         {
-            // var response = new Response();
 
             var trainStationToReturn = _dataContext
                 .TrainStations
@@ -30,28 +29,19 @@ namespace SP23.P01.Web.Controllers
             })
             .ToList();
 
-            // response.Data = trainStationToReturn;
-
             return Ok(trainStationToReturn);
         }
 
         [HttpGet("{trainStationId}")]
         public ActionResult GetById([FromRoute] int trainStationId)
         {
-            // var response = new Response();
 
             var trainStationFromDatabase = _dataContext.TrainStations.FirstOrDefault(x => x.Id == trainStationId);
 
             if (trainStationFromDatabase == null)
             {
-                // response.AddError("trainStationId", "No trainStation found with that Id");
                 return NotFound(new Error("trainStationId", "No trainStation found with that Id"));
             }
-
-            //if (response.HasErrors)
-            //{
-            //    return BadRequest(response);
-            //}
 
             var trainStationToReturn = new TrainStationGetDto
             {
@@ -59,9 +49,6 @@ namespace SP23.P01.Web.Controllers
                 Name = trainStationFromDatabase.Name,
                 Address = trainStationFromDatabase.Address
             };
-
-            //throws error?
-            //response.Data = trainStationToReturn;
 
             return Ok(trainStationToReturn);
         }
